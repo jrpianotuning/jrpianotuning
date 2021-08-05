7aacaa9ce302dec6404b00d8da628a6181379ae6
RATIU5 <jasonmemm@gmail.com>
2021-08-04 19:10:12 -0600

    Fixed button on homepage

--- pages/services.tsx
+++ pages/services.tsx
@@ -52,6 +52,7 @@ export default function Services() {
 						that might damage piano felts and parts, so it feels just right to play for
 						years to come.
 					</p>
+					<BookButton />
 				</ServiceBox>
 				<ServiceBox
 					image="/img/repairs.jpg"
@@ -67,68 +68,8 @@ export default function Services() {
 						twice a year will extend the life of your piano and help your piano sound
 						just right!
 					</p>
+					<BookButton />
 				</ServiceBox>
-				{/* <div className={styles.service_box}>
-					<div className={styles.service_box_content}>
-						<h2>
-							Cleaning <span>$60</span>
-						</h2>
-						<p>
-							Pianos are long-term investments and they can last for decades -
-							that&rsquo;s a long time to gather dust! Over its lifetime, a piano
-							collects dust, insect remains, and perhaps even mice droppings. Regular
-							cleanings will help maintain your piano&rsquo;s sanitation levels and
-							prevent pests that might damage piano felts and parts, so it feels just
-							right to play for years to come.
-						</p>
-					</div>
-					<Image src="img/cleaning.jpg" alt="A dirty piano" width={300} height={300} />
-				</div>
-				<div className={styles.service_box}>
-					<div className={styles.service_box_content}>
-						<h2>
-							Repairs & Regulation <span>$120/hr</span>
-						</h2>
-						<p>
-							With time and use, piano parts may wear out, break, or fall out of
-							alignment. Repairing broken and worn parts and/or regulating a piano so
-							its parts are in alignment makes it more enjoyable to play. At every
-							tuning appointment, I diagnose the piano for needed repairs and
-							regulations that will help your piano fulfill its potential, so you can
-							gather your family around a piano that plays just right!
-						</p>
-					</div>
-					<Image src="img/repairs.jpg" alt="A broken piano" width={300} height={300} />
-				</div> */}
-				{/* <Card title="Tuning">
-					Pianos have incredible musical potential - when they are in tune! Changes in
-					temperature and humidity, moving house, and frequent playing cause pianos to
-					fall out of tune, making them less enjoyable to play. If a piano goes too long
-					without a tuning, it may also require a pitch adjustment before it can hold a
-					tune. Having your piano tuned and serviced once or twice a year will extend the
-					life of your piano and help your piano sound <i>just right</i> all year round!
-				</Card>
-				<Card title="Cleaning">
-					Pianos are long-term investments and they can last for decades - that&apos;s a
-					long time to gather dust! Over its lifetime, a piano collects dust, insect
-					remains, and perhaps even mice droppings. Regular cleanings will help maintain
-					your piano&apos;s sanitation levels and prevent pests that might damage piano
-					felts and parts so it feels <i>just right</i> to play for years to come.
-				</Card>
-				<Card title="Repairs and Regulation">
-					With time and use, piano parts may wear out, break, or fall out of alignment,
-					making the piano less enjoyable or impossible to play. Repairing broken and worn
-					parts and/or regulating a piano so its parts are in alignment makes it more
-					pleasureable to play. At every tuning appointment, our technicians diagnose the
-					piano for possible or necessary repairs and regulations that will help your
-					piano fulfill its potential, as you gather your family around a piano that
-					plays&nbsp;
-					<i>just right</i>!
-				</Card> */}
-			</section>
-
-			<section>
-				<BookButton />
 			</section>
 		</Page>
 	);
